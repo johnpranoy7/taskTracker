463c0d61e4d75828499216e5fd06d2fbc37ca1d2
johnpranoy7 <47387210+johnpranoy7@users.noreply.github.com>
2025-02-07 10:20:50 -0500

    Added Header and Footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { AboutComponent } from './components/about/about.component';
 
 export const routes: Routes = [];
+
+routes.push(
+    { path: '', component: TasksComponent },
+    { path: 'about', component: AboutComponent }
+);
